9454b472755130ef30f08570f83de727fbf7fc0f
PJ Frias <PeejFrias@Gmail.com>
2018-01-19 16:40:56 -0500

    add list_cats method to CLI

--- lib/cat_breeds/cli.rb
+++ lib/cat_breeds/cli.rb
@@ -4,16 +4,30 @@ class CatBreeds::CLI
 	BASE_PATH = "http://www.vetstreet.com/"
 
 	def call
+		puts ""
 		puts "Cats are neat! Check out all the different cat breeds!"
-		make_cats
-		binding.pry
+		cat_breeds = make_cats
+		list_cats(cat_breeds)
+		prompt
 	end
 
 	def make_cats
 		breeds_array = CatBreeds::Scraper.scrape_index(BASE_PATH + "/cats/breeds")
-		breeds_array.each do |breed|
+		breeds_array.collect do |breed|
 			CatBreeds::Cat.new(breed[:name], breed[:page_url])
 		end
 	end
 
+	def list_cats(cat_breeds)
+		puts ""
+		cat_breeds.each.with_index(1) do |b,i|
+			puts "#{i}. #{b.name}"
+		end
+	end
+
+	def prompt
+		puts ""
+		puts "Enter the cat breed or number that you would like to learn more about:"
+		input = gets.strip
+	end
 end
