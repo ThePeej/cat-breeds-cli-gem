cc4b6a1d0830dcc219c34b8ddcda4b8ec953e084
PJ Frias <PeejFrias@Gmail.com>
2018-01-19 23:09:20 -0500

    add more functionality to view_more_details in cli.rb

--- lib/cat_breeds/cli.rb
+++ lib/cat_breeds/cli.rb
@@ -3,7 +3,7 @@ class CatBreeds::CLI
 	# BASE_PATH = "https://www.purina.com/cats/cat-breeds"
 	BASE_PATH = "http://www.vetstreet.com"
 
-	def call
+	def start
 		puts ""
 		puts "Cats are neat! Check out all the different cat breeds!"
 		cat_breeds = make_cats
@@ -31,7 +31,9 @@ class CatBreeds::CLI
 		details = CatBreeds::Scraper.scrape_profile(BASE_PATH + breed.page_url)
 		breed.add_details(details)
 		puts ""
+		puts "----------------------------------------"
 		puts "Overview of the #{breed.name}!"
+		puts "----------------------------------------"
 		puts ""
 		puts "#{breed.blurb}"
 		puts ""
@@ -43,17 +45,62 @@ class CatBreeds::CLI
 
 	def view_more_details(breed)
 		puts "Learn more about the #{breed.name}"
-		puts "1. History"
-		puts "2. Personality"
-		puts "3. Grooming"
-		puts "4. Health"
-		puts "5. Go back to list of all cat"
+		puts "[1] History"
+		puts "[2] Personality"
+		puts "[3] Grooming"
+		puts "[4] Health"
+		puts "[5] [Back] to list of all cat breeds"
 		input = gets.strip
+		topic = nil
+		info = nil
+		case input.downcase
+		when "1","history"
+			topic = "History"
+			info = breed.history
+		when "2","personality"
+			topic = "Personality"
+			info = breed.personality
+		when "3","grooming"
+			topic = "Grooming"
+			info = breed.grooming
+		when "4","health"
+			topic = "Health"
+			info = breed.health
+		when "5", "back"
+			start
+		when "exit"
+			goodbye
+		else
+			puts "Invalid Input. Please try again."
+			view_more_details(breed)
+		end
+		puts ""
+		puts "----------------------------------------"
+		puts "#{topic}"
+		puts "----------------------------------------"
+		puts ""
+		puts "#{info}"
+		puts ""
+		puts "[1] Learn more about the #{breed.name}"
+		puts "[2] Learn about a different cat breed"
+		puts "[exit]"
+		input = gets.strip
+		case input.downcase
+		when "1"
+			view_more_details(breed)
+		when "2","personality"
+			start
+		when "exit"
+			goodbye
+		else
+			puts "Invalid Input. Please try again."
+		end
+
 	end
 
 	def goodbye
 		puts ""
-		puts "Cat's rule!"
+		puts "Cat's rule! See ya!"
 		puts ""
 	end
 
